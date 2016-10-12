a4f0820d9bce855052f338ed96b0e1d7d0c90402
Erick Guanlao <erick.guanlao@milestonegroup.com.au>
2016-10-12 17:12:39 +1100

    Added more files

--- js/app.js
+++ js/app.js
@@ -24,26 +24,47 @@ var app3 = new Vue({
 var app4 = new Vue({
   el: '#app-4',
   data: {
-    todos: [
-      {
-        text: 'Learn JavaScript'
-      }, {
-        text: 'Learn Vue'
-      }, {
-        text: 'Build something awesome'
-      }
-    ]
+    todos: [{
+      text: 'Learn JavaScript'
+    }, {
+      text: 'Learn Vue'
+    }, {
+      text: 'Build something awesome'
+    }]
   }
 });
 
 var app5 = new Vue({
-    el: '#app-5',
-    data: {
-      message: 'Hello Vue.js!'
-    },
-    methods: {
-      reverseMessage: function () {
-        this.message = this.message.split('').reverse().join('');
-      }
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('');
     }
+  }
+});
+
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Hello Vue!'
+  }
+});
+
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+});
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
+  }
 });
